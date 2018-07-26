08a034032ca160d882f5b5397b4566354b1b6021
Katie Weber <k.weber0893@gmail.com>
2018-07-26 09:45:52 -0400

    Partials w locals lab

--- app/models/student.rb
+++ app/models/student.rb
@@ -15,4 +15,8 @@
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
+
+  def self.search(name)
+    where("name like ?", "%#{name}%")
+  end
 end
