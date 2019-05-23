78d614e6f3bb78024973e808361745a8621e7817
Ezz Abousaif <ezzeldin.abousaif@powerhrg.com>
2019-05-23 13:47:09 -0400

    Partial locals lab

--- app/models/student.rb
+++ app/models/student.rb
@@ -15,4 +15,12 @@
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
+
+  def self.search(query)
+    if query.present?
+      where("NAME like ?", "%#{query}%")
+    else
+      all
+    end
+  end
 end
